91d0e4026b42800808fc8113e31894829317d78e
JuanCisnerosM <juan.cisnerosmtz@hotmail.com>
2025-07-31 21:03:51 -0600

    rm

--- PruebasEspecificas/proyecto-pruebas-excelsior/src/main/java/mx/ipn/institucional/aplicaciones/proyectopruebasexcelcior/servicios/UsuarioService.java
+++
@@ -1,9 +0,0 @@
-package main.java.mx.ipn.institucional.aplicaciones.proyectopruebasexcelcior.servicios;
-
-import mx.ipn.institucional.aplicaciones.proyectopruebasexcelcior.modelo.Usuario;
-
-public interface UsuarioService {
-    Usuario obtenerPorId(Long id);
-    Usuario guardar(Usuario usuario);
-    Usuario buscarPorEmail(String email);
-}
